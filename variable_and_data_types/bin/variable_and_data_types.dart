9a67bd83e0ce3bade7670b9d23e4544e74cf61c2
Kaferas <kaferasmart95@gmail.com>
2024-03-07 09:59:12 +0200

    Initial commit

---
+++ variable_and_data_types/bin/variable_and_data_types.dart
@@ -0,0 +1,5 @@
+import 'package:variable_and_data_types/variable_and_data_types.dart' as variable_and_data_types;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variable_and_data_types.calculate()}!');
+}
---
+++ variable_and_data_types/lib/variable_and_data_types.dart
@@ -0,0 +1,4 @@
+int calculate() {
+  // This should return 42 as a result
+  return 6 * 7;
+}
